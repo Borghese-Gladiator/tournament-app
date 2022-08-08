141f6a73578409936e1ab9e54af0ea95d7981b4e
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-08-08 01:28:29 -0400

    Comment out styling

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,21 +1,22 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 
 function Footer() {
-  return (    
-		<AppBar position="static">
-			<Toolbar>
-				<Typography
-					variant="h6"
-					noWrap
-					component="div"
-					sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-				>
-					MUI
-				</Typography>
-				
-			</Toolbar>
-		</AppBar>
-  );
+	return (
+		<Box>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography
+						variant="h6"
+						noWrap
+						component="div"
+						sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+					>
+						MUI
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 }
 
 export default Footer;
--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,18 +1,22 @@
-
-
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { styled } from '@mui/material/styles';
-
-// Custom
+import { AppBar, Box, Toolbar } from '@mui/material';
 import Logo from '@/components/Logo';
 
+function Navbar() {
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+					<Logo />
+					<Box sx={{ flexGrow: 1 }} />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Navbar;
+
+/*
 const AppBarStyled = styled(AppBar)`
 	padding: 8px 16px;
 	transition: width 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
@@ -24,18 +28,4 @@ const AppBarStyled = styled(AppBar)`
 	background: rgba(10, 25, 41, 0.7);
 	color: rgb(160, 170, 180);
 `
-
-function Navbar() {
-  return (
-    <Box>
-      <AppBarStyled position="static">
-        <Toolbar>
-					<Logo />
-					<Box sx={{ flexGrow: 1 }} />
-        </Toolbar>
-      </AppBarStyled>
-    </Box>
-  );
-}
-
-export default Navbar;
+*/
