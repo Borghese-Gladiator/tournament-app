100b06967db2dae2f21eb44c75475f75106dc426
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-08-08 01:57:22 -0400

    Update JoinPage.js

--- client/src/features/join/JoinPage.js
+++ client/src/features/join/JoinPage.js
@@ -1,5 +1,8 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Paper, TextField, Typography } from '@mui/material';
 import { yyyymmdd } from 'utils';
+import GroupIcon from '@mui/icons-material/Group';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 export default function JoinPage() {
 	const title = 'Boston Badminton Tournament 2022';
@@ -9,13 +12,37 @@ export default function JoinPage() {
 	const endDate = new Date(2022, 11, 25);
 	return (
 		<Container maxWidth="md">
-			<Typography variant="h4">{title}</Typography>
+			<Typography variant="h1">Join Tournament</Typography>
+			<Typography variant="h5">{title}</Typography>
 			<Box sx={{ display: 'flex' }}>
-				<Typography variant="subtitle1">{organizer}</Typography>
+				<Typography variant="subtitle1"><GroupIcon />{organizer}</Typography>
 				<Box m={2} />
-				<Typography variant="subtitle1">{location}</Typography>
+				<Typography variant="subtitle1"><LocationOnIcon />{location}</Typography>
 			</Box>
-			<Typography variant="subtitle1">{yyyymmdd(startDate)} - {yyyymmdd(endDate)}</Typography>
+			<Typography variant="subtitle2"><CalendarMonthIcon />{yyyymmdd(startDate)} - {yyyymmdd(endDate)}</Typography>
+			<Container maxWidth="xs">
+				<Paper sx={{ marginTop: 2, display: 'flex', flexDirection: 'column' }}>
+					<TextField
+						id="outlined-name"
+						label="Email Address"
+					/>
+					<Box m={1} />
+					<TextField
+						id="outlined-name"
+						label="First Name"
+					/>
+					<Box m={1} />
+					<TextField
+						id="outlined-name"
+						label="Last Name"
+					/>
+					<Box m={1} />
+					<TextField
+						id="outlined-name"
+						label="Event"
+					/>
+				</Paper>
+			</Container>
 		</Container>
 	)
 }
